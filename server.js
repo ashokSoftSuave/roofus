08e70a6cd59ed179b18b47919191d564117d4d0d
ashokSoftSuave <ashok.kumar.s@softsuave.com>
2023-01-24 15:29:52 +0530

    base route

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./index");
+const router = require("./router");
 
 
-const DB = process.env.Database_connection_string.replace(
-  "<password>",
-  process.env.mongoPassword
-);
+const DB = "mongodb+srv://roofus123:nz3B9aqtoUt7Ii0c@roofusdb.wjsknof.mongodb.net/roofus-db?retryWrites=true&w=majority"
 
 mongoose
   .connect(DB, {
@@ -18,6 +16,8 @@ mongoose
   .then(() => {
     console.log("DataBase connected");
   });
+
+  app.use(router);
 
 
 const port = 400;
